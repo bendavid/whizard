251b5906ba35d75038e426e9ffca395ec604dbf3
Juergen Reuter <whizard@theo-wgs13.desy.de>
2019-11-01 16:53:37 +0100

    136d6df Merge branch 'fix-distcheck-missing-files' into 'master'

---
+++ tests/functional_tests/openloops_12.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+### Testing integration of the real component of ee -> jjj.
+echo "Running script $0"
+if test -f OCAML_FLAG -a -f OPENLOOPS_FLAG -a -f FASTJET_FLAG; then
+    name=`basename @script@`
+    ./run_whizard.sh @script@ --no-logging
+    mv $name.log $name.log.tmp
+    cat $name.log.tmp | sed -e 's/Loading library:.*/Loading library: [...]/' > $name.log
+    diff ref-output/$name.ref $name.log
+else
+    echo "|=============================================================================|"
+    echo "No O'Mega or OpenLoops matrix elements available, test skipped"
+    exit 77
+fi
