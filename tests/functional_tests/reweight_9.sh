1d010e0895e0c6c7ca30ce6c2695bb1c810e81f2
Juergen Reuter <whizard@theo-wgs12.desy.de>
2020-03-02 18:34:44 +0100

    518acfc Merge branch 'fix_version_spec_func_tests' into 'master'

--- tests/functional_tests/reweight_9.sh
+++ tests/functional_tests/reweight_9.sh
@@ -4,6 +4,8 @@ name=`basename @script@`
 echo "Running script $0"
 if test -f OCAML_FLAG; then
     ./run_whizard.sh @script@ --no-logging --no-model
+    cat $name.log | sed -e 's/WHIZARD 2.*$/WHIZARD [version]/' > $name.log.tmp
+    mv $name.log.tmp $name.log
     echo "Contents of ${name}_in.weights.dat" >> $name.log
     cat ${name}_in.weights.dat >> $name.log
     echo "Contents of ${name}_raw.weights.dat" >> $name.log
