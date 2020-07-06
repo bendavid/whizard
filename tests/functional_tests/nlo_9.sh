a02390e1e20a2bafe398343e1e4d4bc0e46dfef6
Juergen Reuter <whizard@theo-wgs12.desy.de>
2020-07-06 21:05:08 +0200

    303f950 Merge branch '374-combined-integration-for-pp-processes' into 'master'

---
+++ tests/functional_tests/nlo_9.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Testing combined NLO calculation of pp -> ee
+echo "Running script $0"
+if test -f OCAML_FLAG ; then
+    name=`basename @script@`
+    ./run_whizard.sh @script@ --no-logging
+    echo "Contents of ${name}_p1.debug:" >> $name.log
+    cat ${name}_p1_fks_regions.out >> $name.log
+    cat ${name}_p1.debug >> $name.log
+    diff ref-output/$name.ref $name.log
+else
+    echo "|=============================================================================|"
+    echo "No O'Mega matrix elements, test skipped"
+    exit 77
+fi
