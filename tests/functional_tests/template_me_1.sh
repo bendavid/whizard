eb14eb8f13fe6c7b36026659aa65120c1ddc4e4b
Juergen Reuter <whizard@naf-theo21.desy.de>
2024-07-13 23:45:49 +0200

    54bd68671 Merge branch 'fix_new_parameters' into 'master'

--- tests/functional_tests/template_me_1.sh
+++ tests/functional_tests/template_me_1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ### Check WHIZARD for a simple test process
 echo "Running script $0"
+rm -f @script@_lib.* @script@_p?.*
 ./run_whizard.sh @script@ --no-logging
 diff ref-output/`basename @script@`.ref `basename @script@`.log
---
+++ tests/functional_tests/template_me_3.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+### Check WHIZARD for a simple test process
+echo "Running script $0"
+rm -f @script@_lib.* @script$_p?.*
+./run_whizard.sh @script@ --no-logging
+name=`basename @script@`
+echo "Contents of ${name}_p1.lhe:" >> $name.log
+cat ${name}_p1.lhe | sed -e 's/^  <generator_version>.*$/[...]/' >> $name.log
+diff ref-output/$name.ref $name.log
