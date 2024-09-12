b1cf3f1150b9c20d02400a0555fad2bf576d8fd0
GitLab Runner <gitlab-runner@>
2024-09-12 23:44:56 +0200

    571e5fcd4 Merge branch 'release_3.1.5' into 'master'

--- tests/functional_tests/template_me_3.sh
+++ tests/functional_tests/template_me_3.sh
@@ -5,5 +5,5 @@ rm -f @script@_lib.* @script$_p?.*
 ./run_whizard.sh @script@ --no-logging
 name=`basename @script@`
 echo "Contents of ${name}_p1.lhe:" >> $name.log
-cat ${name}_p1.lhe | sed -e 's/^  <generator_version>.*$/[...]/' >> $name.log
+cat ${name}_p1.lhe | sed -e 's/^<generator version=.*$/<generator version=[...]>WHIZARD<\/generator>/' >> $name.log
 diff ref-output/$name.ref $name.log
