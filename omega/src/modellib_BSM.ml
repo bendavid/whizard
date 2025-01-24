7eb9d3e9740e57c20ea2f57e96830be52c63f638
GitLab Runner <gitlab-runner@>
2025-01-24 18:25:07 +0100

    4eec6b17e Merge branch 'fix_2hdm' into 'master'

--- omega/src/modellib_BSM.ml
+++ omega/src/modellib_BSM.ml
@@ -7870,9 +7870,9 @@ module TwoHiggsDoublet (Flags : THDM_flags) =
       | "g" -> G Gl
       | "A" -> G Ga | "Z" | "Z0" -> G Z
       | "W+" -> G Wp | "W-" -> G Wm
-      | "h0" -> O Hh
-      | "H0" -> O HH
-      | "A0" -> O HA
+      | "h0" | "Hh" -> O Hh
+      | "H0" | "HH" -> O HH
+      | "A0" | "HA" -> O HA
       | "Hp" | "H+" -> O Hp
       | "Hm" | "H-" -> O Hm
       | _ -> invalid_arg "Modellib_BSM.TwoHiggsDoublet.flavor_of_string"
